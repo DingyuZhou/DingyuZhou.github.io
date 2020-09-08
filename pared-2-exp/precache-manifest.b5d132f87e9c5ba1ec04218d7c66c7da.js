80b798b440cc9bc58d796be651bbb4dc3ae231ac
Dingyu Zhou <dingyu@pared.com>
2020-09-07 22:04:33 -0700

    try to deploy pared-2-exp one more time

---
+++ pared-2-exp/precache-manifest.b5d132f87e9c5ba1ec04218d7c66c7da.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fe5e4a0b97e4ce86ac6e52b2e50849",
+    "url": "/pared-2-exp/index.html"
+  },
+  {
+    "revision": "bddada88b2b1f13671da",
+    "url": "/pared-2-exp/static/css/2.07dca5be.chunk.css"
+  },
+  {
+    "revision": "bddada88b2b1f13671da",
+    "url": "/pared-2-exp/static/js/2.41a97a98.chunk.js"
+  },
+  {
+    "revision": "a575ffc989976f747a7a1c955494d562",
+    "url": "/pared-2-exp/static/js/2.41a97a98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2c21b318caf0a7852f",
+    "url": "/pared-2-exp/static/js/main.eadb58f0.chunk.js"
+  },
+  {
+    "revision": "09355a0b8f71fc4375b0",
+    "url": "/pared-2-exp/static/js/runtime-main.e8aaf714.js"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/pared-2-exp/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/pared-2-exp/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/pared-2-exp/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/pared-2-exp/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/pared-2-exp/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/pared-2-exp/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/pared-2-exp/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/pared-2-exp/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/pared-2-exp/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/pared-2-exp/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/pared-2-exp/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/pared-2-exp/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/pared-2-exp/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/pared-2-exp/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/pared-2-exp/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/pared-2-exp/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
--- pared-2-exp/service-worker.js
+++ pared-2-exp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35b74092a29409ebae722dd58308f871.js"
+  "/pared-2-exp/precache-manifest.b5d132f87e9c5ba1ec04218d7c66c7da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pared-2-exp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
